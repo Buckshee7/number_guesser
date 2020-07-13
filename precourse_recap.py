24a4936e2a4dd69e5f670b82494006b3131713b4
Timothy Saudners <timsaunders88@gmail.com>
2020-07-13 18:49:01 +0100

    Included option to quit mid-game

--- precourse_recap.py
+++ precourse_recap.py
@@ -1,18 +1,28 @@
-# number guessing game using if statements
+# number guessing game using if and conditionals
 
 # generate random number between 1 and 100
 import random
-target = random.randint(0, 101)
+target = random.randint(0, 100)
+give_up = False
+times_guessed = 0
 print(target)
 # get user to guess the number
 guess = int(input("Guess which number between 1 and 100 I'm thinking of: "))
 # if incorrect, loop until user guesses correct number
 while guess != target:
+    # counts how many guesses the user has had and informs them of option to quit after 3 tries
+    times_guessed += 1
+    if times_guessed == 3:
+        print("If you want to quit just pick a number outside of the range")
+    #if user gives up then break
+    if guess < 0 or guess > 100:
+        give_up = True
+        break
     # if guess too high then tell them and get new guess
-    if guess > target:
+    elif guess > target:
         guess = int(input("Too high, try again: "))
-    # if guess is too low then tell them and get new guess
-    elif guess < target:
+    # otherwise guess must be too low so tell them and get new guess
+    else:
         guess = int(input("Too low, try again: "))
-# guess is correct so victory message
-print("Well done, you got it!!")
+# guess is now correct or player quit so provide appropriate ending message
+print("Outside of range. Sorry to see you go :(") if give_up else print("Well done, you got it!!")
